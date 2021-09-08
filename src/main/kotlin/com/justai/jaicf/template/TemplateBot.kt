1dec1f49368c477e145dcee1483fe93e5c9c8a65
mragozina <rm@digintel.ru>
2021-09-08 11:28:16 +0300

    change cailaSettings

--- src/main/kotlin/com/justai/jaicf/template/TemplateBot.kt
+++ src/main/kotlin/com/justai/jaicf/template/TemplateBot.kt
@@ -9,13 +9,17 @@ import com.justai.jaicf.logging.Slf4jConversationLogger
 import com.justai.jaicf.template.scenario.mainScenario
 import java.util.*
 
+/*
 val accessToken: String = System.getenv("JAICP_API_TOKEN") ?: Properties().run {
     load(CailaNLUSettings::class.java.getResourceAsStream("/jaicp.properties"))
     getProperty("apiToken")
 }
+ */
+val accessToken = "e33f77ae-6cb6-4006-a5ec-2d1a5188ee43"
 
 private val cailaNLUSettings = CailaNLUSettings(
-    accessToken = accessToken
+    accessToken = accessToken,
+    confidenceThreshold = 0.2
 )
 
 val templateBot = BotEngine(
